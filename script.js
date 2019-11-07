9a0086ddf2531a1f4996347c0cde1cbc1f5f50d3
Valerii Tabakov <tabakov2@gmail.com>
2019-11-07 19:57:19 +0200

    add styles

---
+++ script.js
@@ -0,0 +1,14 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Выучить JS",
+    text: "Ходить на курс, делать домашки",
+    completed: false
+  },
+  {
+    id: 2,
+    title: "ВыучитьR React",
+    text: "Ходить на курс, делать домашки читать документацию",
+    completed: false
+  }
+];
